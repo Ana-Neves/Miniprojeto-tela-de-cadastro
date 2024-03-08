353d095d3b75cb567373e5b78b33a40de4b47779
Ana-Neves <ana.carolinemms22@gmail.com>
2024-03-08 10:44:26 -0300

    introducao-das-primeiras-funcoes

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ class Data {
         this.email = email;
     }
 }
+
+let dataList = [];
+let formContainer = document.getElementById('formContainer');
+let dataForm = document.getElementById('dataForm');
+let dataListContainer = document.getElementById('dataList');
+
+function showForm() {
+    formContainer.style.display = 'block';
+}
+
+function saveData() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+
+    let newData = new Data(name, email);
+    dataList.push(newData);
+
+    displayDataList();
+    clearForm();
+}
+
+function displayDataList() {
+    dataListContainer.innerHTML = '';
+
+    dataList.forEach((data, index) => {
+        let dataItem = document.createElement('div');
+        dataItem.innerHTML = `<strong>${data.name}</strong> - ${data.email} 
+                             <button onclick="editData(${index})">Editar</button>
+                             <button onclick="deleteData(${index})">Deletar</button>`;
+        dataListContainer.appendChild(dataItem);
+    });
+}
+
